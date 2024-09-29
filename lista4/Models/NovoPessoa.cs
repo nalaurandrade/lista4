112b2bae2682465b3cda4c81ceb7a190964df244
Ana Laura Andrade <analaura140600@gmail.com>
2024-09-29 17:00:07 -0300

    29-09

---
+++ lista4/Models/NovoPessoa.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lista4.Models
+{
+    public class NovoPessoa
+    {
+        [Required(ErrorMessage = "Obrigatório")]
+        [MaxLength(10, ErrorMessage = "Maximo 10 letras ")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Obrigatório")]
+        public string Cf { get; set; }
+        [Required(ErrorMessage = "Obrigatório")]
+
+        public float Peso { get; set; }
+        [Required(ErrorMessage = "Obrigatório")]
+        public float Altura { get; set; }
+        [Required(ErrorMessage = "Altura inválida")]
+
+    }
+}
---
+++ lista4/Services/PessoaRepository.cs
@@ -0,0 +1,25 @@
+﻿using lista4.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lista4.Services
+{
+    public class PessoaRepository : IPessoaRepository
+    {
+        private static List<Pessoa> dadosPessoaList = new List<Pessoa>();
+        public PessoaRepository() { }
+        public void Inserir (Pessoa novaPessoa)
+		{
+			dadosPessoaList.Add(novaPessoa);
+		}
+        public List<Pessoa> ObterTodosPessoa()
+        {
+            return dadosPessoaList;
+        }
+
+        public Pessoa ObterPessoaPorCpf(string cpf)
+        {
+            return dadosPessoaList.FirstOrDefault(p => p.cpf == cpf);
+        }
+    }
+}
